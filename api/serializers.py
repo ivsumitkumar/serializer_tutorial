2efaa7ff429cb9fbbcd70ac7e6d2a77f122d5a65
sumit kumar <vishnusumit2@gmail.com>
2023-08-25 12:53:26 +0530

    Added Validators!

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 from .models import Student
 
+
+#validators
+def checkCaps(value):
+    if value[0] != value[0].upper():
+        raise serializers.ValidationError('First letter must be capital!')
+
 class StudentSerializer(serializers.Serializer):    #used in GET
     id = serializers.IntegerField()
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100,validators=[checkCaps])
     roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
+    city = serializers.CharField(max_length=100,validators=[checkCaps])
 
 class StudentDeserializer(serializers.Serializer):  #used in POST
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100,validators=[checkCaps])
     roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
+    city = serializers.CharField(max_length=100,validators=[checkCaps])
 
     def create(self,validate_data):     #used for POST method
         return Student.objects.create(**validate_data)
@@ -21,3 +27,17 @@ class StudentDeserializer(serializers.Serializer):  #used in POST
         instance.city = validated_data.get('city',instance.city)
         instance.save()
         return instance
+    
+    #field Level Validation
+    def validate_roll(self,value):
+        if value>=200:
+            raise serializers.ValidationError("Seats are full!")
+        return value
+    
+    # Object level validation - used when validation more than one field
+    def validate(self, data):
+        nm = data.get('name')
+        ct = data.get('city')
+        if nm.lower() == 'rohit' and ct.lower() != 'kashmir':
+            raise serializers.ValidationError('city must be Kashmir!')
+        return data
