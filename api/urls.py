1cc1462e43152cbce478cb225191926dee0c46b9
sumit kumar <vishnusumit2@gmail.com>
2023-08-28 11:58:08 +0530

    Simple JWt token usage

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from . import views
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from . import views
+
+#creating a router
+router = DefaultRouter()
+
+#Register Student Viewse with Router
+router.register('studentapi',views.StudentModelViewSet,basename='student')
+
+
 urlpatterns = [
-    path('studentapi/',views.StudentAPI.as_view()), #used in class view api
+    path('', include(router.urls)),
     path('gettoken/',TokenObtainPairView.as_view(),name='get_token'),
     path('refreshtoken/',TokenRefreshView.as_view(),name='refresh_token'),
     path('verifytoken/',TokenVerifyView.as_view(),name='verify_token'),
--- serializer_tutorial/settings.py
+++ serializer_tutorial/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(hours=5),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=3),
+    'ROTATE_REFRESH_TOKENS':True
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
