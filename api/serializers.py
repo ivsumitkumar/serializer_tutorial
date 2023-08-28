f61b858b689ae4d575fba77557ff58c54ca615a3
sumit kumar <vishnusumit2@gmail.com>
2023-08-28 20:50:32 +0530

    Generic api view and mixins

--- api/serializers.py
+++ api/serializers.py
@@ -3,37 +3,7 @@ from .models import Student
 
 
 # Model Serializer
-class StudentDeserializer(serializers.ModelSerializer):
-    #validators
-    def checkCaps(value):
-        if value[0] != value[0].upper():
-            raise serializers.ValidationError('First letter must be capital!')
-    name = serializers.CharField(validators=[checkCaps]) # adding validators method 1
-
+class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name','roll','city']
-        # read_only_fields = ['name','roll']    # adding validators method 2
-        # extra_kwargs = {'name':{'read_only':True} # adding validators method 3
-
-    # Field level validation
-    def validate_roll(self,value):
-        if value >= 200:
-            raise serializers.ValidationError("Seats are full!")
-        return value
-    
-    # Object level Validation
-    def validate(self,value):
-        nm = value.get('name')
-        ct = value.get('city')
-        if nm.lower() == 'rohit' and ct.lower() != 'kashmir':
-            raise serializers.ValidationError('city must be Kashmir!')
-        return value
-
-
-
-class StudentSerializer(serializers.Serializer):    #used in GET
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=100)
-    roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
+        fields = ['id','name','roll','city']
--- api/views.py
+++ api/views.py
@@ -1,67 +1,30 @@
-from django.http import HttpResponse, JsonResponse
 from .models import Student
-from .serializers import  StudentDeserializer,StudentSerializer
-from rest_framework.renderers import JSONRenderer 
-import io #used in DeSerializer
-from rest_framework.parsers import JSONParser #used in deserializer
-from django.views.decorators.csrf import csrf_exempt #to bypass csrf in function based views
+from .serializers import StudentSerializer
 
-from django.utils.decorators import method_decorator    # used to bypass csrf in class based view
-from django.views import View   #used in class based view
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
-# Create your views here.
 
-# class based view
-@method_decorator(csrf_exempt, name='dispatch')
-class StudentAPI(View):
-    def get(self,request, *args, **kwargs):
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        native_data = JSONParser().parse(stream)
-        id = native_data.get('id',None)
-        if id is not None:
-            stu = Student.objects.get(id=id)
-            stu_serializer = StudentSerializer(stu)
-            return JsonResponse(stu_serializer.data,safe=False)
-        stu = Student.objects.all()
-        stu_serializer = StudentSerializer(stu,many=True)
-        return JsonResponse(stu_serializer.data,safe=False)
-    
-    def post(self,request, *args, **kwargs):
-        json_data = request.body            # takes json data
-        stream = io.BytesIO(json_data)      # Make stream of json data i.e parsing json data
-        native_data = JSONParser().parse(stream)    #parsing of data to native datatype
-        deserializer = StudentDeserializer(data=native_data) #converting native to complex type or model instances
-        if deserializer.is_valid():         # validating data
-            deserializer.save()             # saving data
-            res  = {"msg":"Data inserted!"}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data,content_type = 'application/json')
-        json_data=JSONRenderer().render(deserializer.errors)
-        return HttpResponse(json_data,content_type='application/json')
-    
-    def put(self,request,*args, **kwargs):
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        native_data = JSONParser().parse(stream)
-        id=native_data.get('id')
-        stu = Student.objects.get(id=id)
-        stu_serializer = StudentDeserializer(stu,data=native_data,partial=True)
-        if stu_serializer.is_valid():
-            stu_serializer.save()
-            res={"msg":"Data Updated"}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data,content_type='application/json')
-        json_data = JSONRenderer().render(stu_serializer.errors)
-        return HttpResponse(json_data,content_type='application/json')
+class StudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def delete(self,request,*args, **kwargs):
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        native_data = JSONParser().parse(stream)
-        id = native_data.get('id')
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-        stu = Student.objects.get(id=id)
-        stu.delete()
-        res={'msg':'Entry Deleted!'}
-        return JsonResponse(res,safe=False)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
