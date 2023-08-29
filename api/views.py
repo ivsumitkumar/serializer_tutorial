7a92e963010dc66e369d879d2fec4cc412734f93
sumit kumar <vishnusumit2@gmail.com>
2023-08-29 22:39:14 +0530

    ModelViewset in views implemented

--- api/views.py
+++ api/views.py
@@ -1,51 +1,9 @@
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from django.shortcuts import render
 
-class StudentAPI(viewsets.ViewSet):
-    def list(self,request):
-        stu = Student.objects.all()
-        stu_serializer = StudentSerializer(stu,many = True)
-        return Response(stu_serializer.data)
-    
-    def retrieve(self, request, pk=None):
-        id=pk
-        if id is not None:
-            stu = Student.objects.get(id=id)
-            stu_serializer = StudentSerializer(stu)
-            return Response(stu_serializer.data)
-        
-    def create(self,request):
-        stu_serailizer = StudentSerializer(data = request.data)
-        if stu_serailizer.is_valid():
-            stu_serailizer.save()
-            return Response({'msg':'Data Created'},status=status.HTTP_201_CREATED)
-        return Response(stu_serailizer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def update(self, request, pk):
-        id = pk
-        stu = Student.objects.get(id=id)
-        stu_serializer = StudentSerializer(stu, data=request.data)
-        if stu_serializer.is_valid():
-            stu_serializer.save()
-            return Response({'msg':'Data Updated'},status=status.HTTP_202_ACCEPTED)
-        return Response(stu_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def partial_update(self, request, pk):
-        id = pk
-        stu = Student.objects.get(id=id)
-        stu_serializer = StudentSerializer(stu, data=request.data, partial=True)
-        if stu_serializer.is_valid():
-            stu_serializer.save()
-            return Response({'msg':'Data Updated'},status=status.HTTP_202_ACCEPTED)
-        return Response(stu_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk=None):
-        id = pk
-        stu = Student.objects.get(id=id)
-        stu.delete()
-        return Response({'msg':'Data Deleted!'},status=status.HTTP_202_ACCEPTED)
+class StudentAPI(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
     
