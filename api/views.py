aaf73254730c35d130cfc266a620144f61501a45
sumit kumar <vishnusumit2@gmail.com>
2023-08-29 18:08:41 +0530

    Concrete api view implemented

--- api/views.py
+++ api/views.py
@@ -1,30 +1,13 @@
 from .models import Student
 from .serializers import StudentSerializer
 
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-
-class StudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
-    queryset = Student.objects.all()
+class StudentAPI(ListCreateAPIView):
+    queryset = Student.objects.all()    # use .all() or .get_queryset()
     serializer_class = StudentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    queryset = Student.objects.all()
+class RUDStudentAPI(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.get_queryset()
     serializer_class = StudentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
