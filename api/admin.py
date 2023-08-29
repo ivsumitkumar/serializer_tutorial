1fea3aa559a6080117f0bed2c23df99a1af41bec
sumit kumar <vishnusumit2@gmail.com>
2023-08-29 17:27:48 +0530

    managing user/admin  permissions

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Student
 
+
 # Register your models here.
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['id','name','roll', 'city']
+    
+    def get_form(self,request, obj=None, **kwargs):
+        form = super().get_form(request,obj,**kwargs)
+        is_superuser = request.user.is_superuser
+
+        if is_superuser:
+            form.base_fields['name'].disabled = True
+        return form
