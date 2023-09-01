12db0c5339823c4e94aad765f18847de0f510da3
sumit kumar <vishnusumit2@gmail.com>
2023-09-01 16:14:28 +0530

    custom authentication in browsable api

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-# from rest_framework.authentication import TokenAuthentication
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from .customAuth import CustomAuthentication
 
 class StudentAPI(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [CustomAuthentication]
+    permission_classes=[IsAuthenticated]
