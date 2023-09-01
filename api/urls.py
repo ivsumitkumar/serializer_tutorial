ace50352c080b3d65a41ccc13b8fe0930fcb63a1
sumit kumar <vishnusumit2@gmail.com>
2023-09-01 15:51:01 +0530

    Completed

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,7 @@ router = DefaultRouter()
 router.register('studentapi',views.StudentAPI, basename='student')
 
 urlpatterns = [
-    path('studentapi/',views.studentapi),
-    path('studentapi/<int:pk>',views.studentapi),
+    path('',include(router.urls)),
     path('auth/',include('rest_framework.urls', namespace='log_in_out')),
     # path('gettoken/',obtain_auth_token),  # used in CLI token generator
 ]
