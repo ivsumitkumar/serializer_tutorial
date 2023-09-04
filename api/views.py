0a1a9648fdf79cb047543af1f7f56d1dea3f46d2
sumit kumar <vishnusumit2@gmail.com>
2023-09-04 15:12:01 +0530

    Ordering filter applied

--- api/views.py
+++ api/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from api.models import Student
 from api.serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
-from rest_framework.filters import SearchFilter
+# from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 # from rest_framework.throttling import ScopedRateThrottle
 
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['name']
+    # filter_backends = [SearchFilter]
+    # search_fields = ['name','city']
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['name']
