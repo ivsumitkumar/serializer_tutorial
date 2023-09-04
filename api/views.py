0ad2d091904a4b0a2f9f9c36a824ce2965420c67
sumit kumar <vishnusumit2@gmail.com>
2023-09-04 18:58:26 +0530

    pagination COmpleted!

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
-from .models import Student
-from .serializers import StudentSerializer
-
+from api.models import Student
+from api.serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
+from api.myPagination import myPaginations  # local pagination
+
+
+#   for global settings of pagination check settings.py file
 
 class StudentAPI(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = myPaginations    # local pagination
+
